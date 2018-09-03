5ab565d60b53470b632584f9252395e970596dfe
parkminho <minho4377@daum.net>
2018-09-03 20:51:06 +0900

    initial commit

---
+++ components/Timer/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, StatusBar } from "react-native";
+import Button from '../Button';
+
+class Timer extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <View style={[styles.upper, styles.upper]}>
+                    <Text style={styles.time}>25:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Button iconName="play-circle" onPress={() => alert("it works!")}  />
+                    <Button iconName="stop-circle" onPress={() => alert("it works!")}  />
+                </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#CE0B24"
+    },
+    upper: {
+        flex: 2,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    lower: {
+        flex: 2,
+        justifyContent:"center",
+        alignItems: "center"
+    },
+    time: {
+        color: "white",
+        fontSize: 120,
+        fontWeight: "100"
+    }
+});
+export default Timer;
---
+++ reducer.js
@@ -0,0 +1,95 @@
+// import
+
+
+// Actions
+
+const START_TIMER = 'START_TIMER';
+const RESTART_TIMER = 'RESTART_TIMER';
+const ADD_SEOND = 'ADD_SECOND';
+
+// Action Creators
+
+function startTimer(){
+    return {
+        type: START_TIMER
+    };
+}
+
+function restartTimer(){
+    return{
+        type: RESTART_TIMER
+    };  
+}
+
+function addSecond(){
+    return {
+        type: ADD_SEOND
+    };
+}
+
+
+// Reducer
+
+const TIME_DURATION = 1500;
+
+const initialState = {
+    isPlaying: false,
+    elapsedTime: 0,
+    timerDuration: TIME_DURATION
+}
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case START_TIMER:
+            return applyStartTimer(state, action);
+        case RESTART_TIMER:
+            return applyStartTimer(stat, action);
+        case ADD_SEOND:
+            return applyStartTimer(state);
+    }
+}
+
+// Reducer Functions
+
+function applyStartTimer(state){
+    return{
+        ...state,
+        isPlaying: true,
+
+    };
+    
+}
+
+function applyRestartTimer(state){
+    return{
+        ...state,
+        isPlaying: false,
+        elapsedTime: 0
+    };
+}
+
+function applyAddSecond(state){
+    if(state.elapsedTime < TIMER_DURATION){
+        return {
+            ...state,
+            elapsedTime: sate.elapsedTime + 1
+        };
+    } else {
+        return {
+            ...state,
+            isPlaying: false
+        };
+    }
+}
+// Export Action Creators
+
+const actionCreators = {
+    startTimer,
+    restartTimer,
+    addSecond
+}
+
+
+// Export Reducer
+
+export default reducer;
